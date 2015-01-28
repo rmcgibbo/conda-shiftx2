15b4ed8896482b391f4015845311e210da6c9a92
Robert T. McGibbon <rmcgibbo@gmail.com>
2015-01-28 03:02:13 +0100

    CHanges

--- shiftx2/build.sh
+++ shiftx2/build.sh
@@ -5,6 +5,6 @@ cat <<EOF > $PREFIX/bin/shiftx2.py
 #!/usr/bin/env python
 import sys
 import os
-os.chdir('$PREFIX/share/shiftx2')
-os.system(' '.join(['./shiftx2.py'] + sys.argv[1:]))
+os.environ['PYTHONPATH'] = '$PREFIX/share/shiftx2'
+os.system(' '.join(['/usr/bin/python2', '$PREFIX/share/shiftx2/shiftx2.py'] + sys.argv[1:]))
 EOF
